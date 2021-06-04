ef592f4a93bd7cc31a716492910f62951343754b
tyler509499 <60471582+tyler509499@users.noreply.github.com>
2021-06-04 15:45:59 +0300

    Update APIkeys.swift

--- WeatherAppFirecode/Constants/APIkeys.swift
+++ WeatherAppFirecode/Constants/APIkeys.swift
@@ -8,6 +8,6 @@
 import Foundation
 
 struct APIkeys {
-    static let googlePlacesKey = "AIzaSyCKOZezx8bU9yW99VBdDMyYpz4eX4TcINw"
-    static let openWeatherKey = "0404a7847a272e2bbfae0c995bcff372"
+    static let googlePlacesKey = ""
+    static let openWeatherKey = ""
 }
